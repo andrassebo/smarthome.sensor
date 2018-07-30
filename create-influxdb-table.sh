c43089b7147ca49a59ce61e44917e816ae3cdca4
Andras Sebo <andras.sebo@gmail.com>
2018-07-30 21:29:25 +0200

    added retention policy

--- create-influxdb-table.sh
+++ create-influxdb-table.sh
@@ -1,1 +1,2 @@
 curl -i -XPOST http://localhost:8086/query --data-urlencode "q=CREATE DATABASE sensor"
+curl -i -XPOST http://localhost:8086/query --data-urlencode "q=CREATE RETENTION POLICY "forever" ON "sensor" DURATION inf REPLICATION 1 DEFAULT"
